743b3255ad763111756a0da269dc57bf0ad686a2
MCManuelLP <mcmanuellp@gmail.com>
2018-05-09 20:41:39 +0200

    Refactored js cookie handling

--- visitorcount.js
+++ visitorcount.js
@@ -1,5 +1,10 @@
 "use strict";
 
+let uid = function(a) { // https://gist.github.com/jed/982883
+	return a ? (a ^ Math.random() * 16 >> a / 4).toString(16) :
+		([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, uid);
+}
+
 let counter = function(method, ondone) {
 	let req = new XMLHttpRequest();
 	req.onreadystatechange = function() {
@@ -17,33 +22,19 @@ let counter = function(method, ondone) {
 
 // http://ec.europa.eu/ipg/basics/legal/cookies/index_en.htm
 
-let cookies = function() {
-	let visitor_uid = '1234'; // TODO generate somehow
-
+let cookieConsent = function() {
 	let consent = Cookies.get("CookieConsent");
 
 	if (consent) { // consent cookie set
-		let cookie_settings = {}
-
-		if (consent === "true") { // consent given
-			// permanent cookie, expires after one year (maximum expire time compliant with eu law)
-			cookie_settings.expires = 365;
-		} else { // consent not given
-			// session cookie, when no expire time is set, the cookie will expire on browser exit
-		}
-		
-		if (Cookies.get("VisitorUID")) { // not a new visitor
-			showCounter(); // just show visitor counter
-		} else { // visitor_uid cookie not set, we asume this is a new visitor
-			Cookies.set("VisitorUID", visitor_uid, cookie_settings);
-			counter("POST", showCounter); // send increment to server and show counter when the post finishes
-		}
+		manageCookies();
 	} else { // consent cookie not set
-		document.querySelector("#cookie-notice").classList.remove("hidden"); // show cookie message
+		// show cookie message
+		document.querySelector("#cookie-notice").classList.remove("hidden");
 		
 		let oninteract = function() {
-			document.querySelector("#cookie-notice").classList.add("hidden"); // hide cookie message
-			cookies(); // rerun this method
+			// hide cookie message
+			document.querySelector("#cookie-notice").classList.add("hidden");
+			manageCookies();
 		};
 		
 		document.querySelector("#cookie-notice #accept-cn").addEventListener("click", function() {
@@ -55,12 +46,45 @@ let cookies = function() {
 			oninteract();
 		});
 	}
-};
+}
+
+let manageCookies = function() {
+	let consent = Cookies.get("CookieConsent");
+	
+	let cookie_settings = {}
+
+	if (consent === "true") { // consent given
+		// permanent cookie: expires after one year (maximum expire time compliant with eu law)
+		cookie_settings.expires = 365;
+		// refresh expire time of consent cookie
+		Cookies.set("CookieConsent", "true", {expires: 365});
+	} else { // consent not given
+		// session cookie: when no expire time is set, it should expire on browser exit (depends on browser implementation)
+	}
+	
+	let visitor_uid = Cookies.get("VisitorUID");
+
+	if (visitor_uid) { // not a new visitor
+		// just show visitor counter
+		showCounter();
+	} else { // visitor_uid cookie not set, we asume this is a new visitor
+		// generate new random uid
+		visitor_uid = uid();
+		// send increment to server and show counter when that finishes
+		counter("POST", showCounter);
+	}
+	
+	// set/refresh cookies to extend expire time
+	Cookies.set("CookieConsent", consent, cookie_settings);
+	Cookies.set("VisitorUID", visitor_uid, cookie_settings);
+}
 
 let showCounter = function() {
-	counter("GET", function(req) { // request visitor count
+	// request visitor count from server
+	counter("GET", function(req) {
+		// set count to value received from server
 		document.querySelector("#counter span").textContent = req.responseText;
 	});
 };
 
-document.addEventListener("DOMContentLoaded", cookies);
+document.addEventListener("DOMContentLoaded", cookieConsent);
